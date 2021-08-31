37ffda704c9f3a623ff8454d9dbbcc45ccc21555
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-31 20:18:43 +0530

    feat:Added Csv Files For Read And Write operation

---
+++ Day23_AddressBookSystem/IContacts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day23_AddressBookSystem
+{
+    interface IContacts
+    {
+        public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber, string bookName);
+        public void EditContact(string name, string bookName);
+        public void DeleteContact(string deletename, string bookName);
+        public void AddAddressBook(string bookName);
+        public void DisplayContact(string bookName);
+
+    }
+}
---
+++ Day23_AddressBookSystem/ReadWriteFileIO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day23_AddressBookSystem
+{
+    class ReadWriteFileIO
+    {
+        static string file = @"C:\Users\HP\csharp\Day23_AddressBookSystem\Day23_AddressBookSystem\AddressBook.txt";
+
+        public void WriteToFile(Dictionary<string, AddressBookBuider> addressBookDictionary)
+        {
+            StreamWriter writer = new StreamWriter(file, true);
+            foreach (AddressBookBuider item in addressBookDictionary.Values)
+            {
+                foreach (Contacts contact in item.addressBook.Values)
+                {
+                    writer.WriteLine(contact.ToString());
+                }
+            }
+            Console.WriteLine("\nSuccessfully added to Text file.");
+            writer.Close();
+        }
+        public void ReadFromFile()
+        {
+            Console.WriteLine(File.ReadAllText(file));
+        }
+    }
+}

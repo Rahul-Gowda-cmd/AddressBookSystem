11d1e63c448950ee402101635cef442de23132f2
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-09-09 14:58:43 +0530

    feat:Retriving all data Using Ado.net Connection

---
+++ AddressBook_DataBase/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBook_DataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookDatabases ab = new AddressBookDatabases();
+            ab.GetPersonDetailsfromDatabase();
+        }
+    }
+}
